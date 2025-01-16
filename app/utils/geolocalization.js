693f6872aefaff6002f0e6bde3ccc65625e5935b
gdychala <g.dychala@gmail.com>
2025-01-17 00:36:30 +0100

    test8

--- app/utils/geolocalization.js
+++ app/utils/geolocalization.js
@@ -1,27 +1,26 @@
 'use client';
-export const watchGeolocation = (onSuccess, onError, options = {}) => {
-    if (!("geolocation" in navigator)) {
-      onError("Geolokalizacja nie jest wspierana przez tę przeglądarkę.");
-      return null;
-    }
-  
-    const watchId = navigator.geolocation.watchPosition(
-      (position) => {
-        onSuccess({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-        });
-      },
-      (err) => {
-        onError(`Błąd geolokalizacji: ${err.message}`);
-      },
-      options
-    );
-  
-    // Zwracamy funkcję do zatrzymania śledzenia
-    return () => {
-      navigator.geolocation.clearWatch(watchId);
-    };
+export const watchGeolocation = (onSuccess, onError, options) => {
+  if (!("geolocation" in navigator)) {
+    onError("Geolokalizacja nie jest wspierana przez tę przeglądarkę.");
+    return null;
+  }
+
+  const watchId = navigator.geolocation.watchPosition(
+    (position) => {
+      onSuccess({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      });
+    },
+    (err) => {
+      onError(`Błąd geolokalizacji: ${err.message}`);
+    },
+    options
+  );
+
+  // Zwracamy funkcję do zatrzymania śledzenia
+  return () => {
+    navigator.geolocation.clearWatch(watchId);
   };
-  
-  
+};
+
