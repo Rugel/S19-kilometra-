57b02c792c42758336f9b6569750cacd6303281c
gdychala <g.dychala@gmail.com>
2025-01-16 23:46:55 +0100

    test7

--- app/page.tsx
+++ app/page.tsx
@@ -379,14 +379,17 @@ export default function Home() {
 }*/
 'use client';
 import React from 'react';
-import OpenMap from "./components/OpenMap";
+//import OpenMap from "./components/OpenMap";
 import MapsSelect from "./components/MapsSelect";
 import GeoLink from "./components/GeoLink";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Test from "./components/testStart";
+import dynamic from 'next/dynamic';
 
-
+const OpenMap = dynamic(() => import('./components/OpenMap'), {
+  ssr: false, // Wyłączenie renderowania na serwerze
+});
 
 export default function Home() {
   
