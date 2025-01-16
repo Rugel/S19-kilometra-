49c263f8a563e23c3f438be63463c8db1fbffdea
gdychala <g.dychala@gmail.com>
2025-01-16 22:30:36 +0100

    test2

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+/*'use client';
 import React from "react";
 import OpenMap from "./components/OpenMap";
 import MapsSelect from "./components/MapsSelect";
@@ -66,4 +66,76 @@ export default function Home() {
       </div>
     );
   }
+}*/
+
+'use client';
+import React, { useState, useEffect, useCallback } from "react";
+import OpenMap from "./components/OpenMap";
+import MapsSelect from "./components/MapsSelect";
+import GeoLink from "./components/GeoLink";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { watchGeolocation } from "./utils/geolocalization";
+
+export default function Home() {
+  const [location, setLocation] = useState(null);
+  const [error, setError] = useState(null);
+  const [isTracking, setIsTracking] = useState(false);
+  const [stopTracking, setStopTracking] = useState<null | (() => void)>(null);
+
+  // Funkcja do rozpoczęcia śledzenia lokalizacji
+  const startTracking = useCallback(() => {
+    setIsTracking(true);
+    const stop = watchGeolocation(
+      (position: React.SetStateAction<null>) => setLocation(position),
+      (err: React.SetStateAction<null>) => setError(err),
+      { enableHighAccuracy: true }
+    );
+    setStopTracking(() => stop); // Przechowujemy funkcję zatrzymującą
+  }, []);
+
+  // Funkcja do zatrzymania śledzenia lokalizacji
+  const stopTrackingLocation = useCallback(() => {
+    if (stopTracking) stopTracking();
+    setIsTracking(false);
+  }, [stopTracking]);
+
+  // Zatrzymywanie śledzenia przy odmontowaniu komponentu
+  useEffect(() => {
+    return () => {
+      if (stopTracking) stopTracking();
+    };
+  }, [stopTracking]);
+
+  return (
+    <div id="contener">
+      <div id="content">
+        <Header />
+        <div className="info">
+          <button className="start" onClick={startTracking} disabled={isTracking}>
+            Rozpocznij
+          </button>
+          <button className="start" onClick={stopTrackingLocation} disabled={!isTracking}>
+            Zatrzymaj
+          </button>
+          <div>
+            <p id="result">
+              Szerokość: <span className="data">{location ? location.latitude : 'brak danych'}</span>
+            </p>
+            <p className="normal">
+              Długość: <span className="data">{location ? location.longitude : 'brak danych'}</span>
+            </p>
+          </div>
+          {error && <p>Błąd: {error}</p>}
+        </div>
+        <MapsSelect />
+        <GeoLink />
+      </div>
+      <div id="map">
+        <OpenMap />
+      </div>
+      <Footer />
+    </div>
+  );
 }
+
