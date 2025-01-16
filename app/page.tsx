ed4b845d2467b15406dbea2a7ea6e1322074c66d
gdychala <g.dychala@gmail.com>
2025-01-16 23:25:51 +0100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,6 @@ import MapsSelect from "./components/MapsSelect";
 import GeoLink from "./components/GeoLink";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import { useState, useEffect } from 'react';
-import { watchGeolocation } from "./utils/geolocalization";
 //import dynamic from "next/dynamic";
 
 
@@ -380,69 +378,24 @@ export default function Home() {
   );
 }*/
 'use client';
-import React, { useState, useEffect, useCallback } from "react";
+import React from 'react';
 import OpenMap from "./components/OpenMap";
 import MapsSelect from "./components/MapsSelect";
 import GeoLink from "./components/GeoLink";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import { watchGeolocation } from "./utils/geolocalization";
+import Test from "./components/testStart";
 
-type Location = {
-  latitude: number;
-  longitude: number;
-};
+
 
 export default function Home() {
-  const [location, setLocation] = useState<Location | null>(null);
-  const [error, setError] = useState<string | null>(null);
-  const [isTracking, setIsTracking] = useState(false);
-  const [stopTracking, setStopTracking] = useState<null | (() => void)>(null);
-
-  const startTracking = useCallback(() => {
-    if (typeof window !== "undefined") {
-      setIsTracking(true);
-      const stop = watchGeolocation(
-        (position: Location) => setLocation(position),
-        (err: { message: string }) => setError(err.message || "Wystąpił błąd"),
-        { enableHighAccuracy: true }
-      );
-      setStopTracking(() => stop);
-    }
-  }, []);
-
-  const stopTrackingLocation = useCallback(() => {
-    if (stopTracking) stopTracking();
-    setIsTracking(false);
-  }, [stopTracking]);
-
-  useEffect(() => {
-    return () => {
-      if (stopTracking) stopTracking();
-    };
-  }, [stopTracking]);
+  
 
   return (
     <div id="contener">
       <div id="content">
         <Header />
-        <div className="info">
-          <button className="start" onClick={startTracking} disabled={isTracking}>
-            Rozpocznij
-          </button>
-          <button className="start" onClick={stopTrackingLocation} disabled={!isTracking}>
-            Zatrzymaj
-          </button>
-          <div>
-            <p id="result">
-              Szerokość: <span className="data">{location ? location.latitude : 'brak danych'}</span>
-            </p>
-            <p className="normal">
-              Długość: <span className="data">{location ? location.longitude : 'brak danych'}</span>
-            </p>
-          </div>
-          {error && <p>Błąd: {error}</p>}
-        </div>
+        <Test />
         <MapsSelect />
         <GeoLink />
       </div>
