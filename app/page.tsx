4bc31e9157ebdc41de9091230deca3a723d5b6c4
gdychala <g.dychala@gmail.com>
2025-01-18 16:57:05 +0100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import GeoLink from "./components/GeoLink";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 //import Localization from "./components/Localization";
-import dynamic from 'next/dynamic';
+//import dynamic from 'next/dynamic';
 import MapComponent from './components/MapCom'
 
 //const OpenMap = dynamic(() => import('./components/OpenMap'), { ssr: false, });
