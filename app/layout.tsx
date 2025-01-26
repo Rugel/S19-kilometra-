fd00fca80c552334c83aabd4e9af16dac039857f
gdychala <g.dychala@gmail.com>
2025-01-26 23:51:36 +0100

    added GTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,22 @@ export default function RootLayout({
   return (
     <html>
       <head>
+      <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=GTM-5BG6KQNM`}
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'GTM-5BG6KQNM', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        ></script>
       <meta name="apple-mobile-web-app-title" content="S19 - kilometraż" />
       </head>
       <body>
