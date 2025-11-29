ce1c25463172dbf5fac5a2d919003efff2909e5a
gdychala <g.dychala@gmail.com>
2025-11-29 19:16:47 +0100

    fix

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -6,7 +6,7 @@ const year = data.getFullYear();
 export default function Footer() {
     return (
         <footer id="footer">
-            &copy; 2024 S19 Kock - Wola Skromowska
+            &copy; {year} *** S19 Kock - Wola Skromowska ***
         </footer>
     );
 }
