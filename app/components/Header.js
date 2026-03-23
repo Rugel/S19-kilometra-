6d4a6e69226e8b6d29efd4970d3a488675c2739f
Gzegorz <g.dychala@gmail.com>
2026-03-23 22:48:27 +0100

    styled menu

--- app/components/Header.js
+++ app/components/Header.js
@@ -15,7 +15,7 @@ export default function Header() {
         <header className="header">
             <nav className="nav-menu">
                 <Link href="/" className="logo-link" title="Strona główna">
-                    <Image src={logo} alt="S19 Logo" width={28} height={28} priority />
+                    <Image src={logo} alt="S19 Logo" width={36} height={36} priority />
                 </Link>
                 <Link href="/" className={getLinkClass('/')}>Obwodnica Kocka</Link>
                 <Link href="/radzyn-kock" className={getLinkClass('/radzyn-kock')}>Radzyń Podlaski - Kock</Link>
